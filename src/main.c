c493c01be5fc1468ece9c7c991b9c02dc9453763
Luiz Costa <lhlc@cesar.school>
2026-09-11 00:12:35 -0300

    tokenização e estrutura básica do código

---
+++ src/main.c
@@ -0,0 +1,174 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+
+typedef struct tarefa {
+    char nome[50];
+    long PERIODO;
+    long BURST;
+    long DEADLINE;
+} tarefa;
+
+int converter_long(const char *str, long *saida) {
+
+    errno = 0;
+
+    char *endptr;
+
+    long valor = strtol(str, &endptr, 10);
+
+    if (*endptr != '\0' && *endptr != '\n' && *endptr != '\r' && *endptr != ' ') {
+        return 0;
+    }
+
+    if (errno == ERANGE) {
+        return 0;
+    }
+
+    if (valor <= 0) {
+        return 0;
+    }
+
+    *saida = valor;
+
+    return 1;
+}
+
+int parse_argumentos(char *args, tarefa *novaTarefa) {
+
+    int contador = 0;
+
+    char *token = strtok(args, " \t\n\r");
+
+    if (token == NULL) {
+        return 0;
+    }
+
+    strcpy(novaTarefa->nome, token);
+
+    token = strtok(NULL, " \t\n\r");
+
+    while (token != NULL) {
+
+        if (contador >= 3) {
+            fprintf(stderr, "Error: Numero de argumentos invalido. Esperado 4 argumentos.\n");
+            return 0;
+        }
+
+        if (contador == 0) {
+            if (!converter_long(token, &novaTarefa->PERIODO)) {
+                return 0;
+            }
+        } else if (contador == 1) {
+            if (!converter_long(token, &novaTarefa->DEADLINE)) {
+                return 0;
+            }
+        } else if (contador == 2) {
+            if (!converter_long(token, &novaTarefa->BURST)) {
+                return 0;
+            }
+        }
+
+        contador++;
+
+        token = strtok(NULL, " \t\n\r");
+    }
+
+    if (contador != 3) {
+        fprintf(stderr, "Error: Numero de argumentos invalido. Esperado 4 argumentos.\n");
+        return 0;
+    }
+
+    if (contador == 3) {
+
+        if(!(novaTarefa->BURST <= novaTarefa->DEADLINE && novaTarefa->DEADLINE <= novaTarefa->PERIODO)) {
+            fprintf(stderr, "Error: Argumentos invalidos. Esperado BURST <= DEADLINE <= PERIODO.\n");
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+
+int main(int argc, char *argv[]) {
+    
+    tarefa listaTarefas[100];
+    int contadorTarefas = 0;
+
+    if (argc != 3) {
+        fprintf(stderr, "Error: <./scheduler> <modo> <arquivo.txt>\n");
+        exit(1);
+    }
+    
+    if(strcmp(argv[1], "rate") == 0) {
+    
+    } else if(strcmp(argv[1], "edf") == 0) {
+    
+    } else {
+        fprintf(stderr, "Error: Modo invalido. Use 'rate' ou 'edf'.\n");
+        exit(1);
+    }
+
+    if (strlen(argv[2]) < 4) {
+        fprintf(stderr, "Error: Nome de arquivo invalido.\n");
+        exit(1);
+        
+    } else {
+        FILE *arquivo = fopen(argv[2], "r");
+        if (arquivo == NULL) {
+            fprintf(stderr, "Error: Nao foi possivel abrir o arquivo '%s'.\n", argv[2]);
+            exit(1);
+        }
+        
+        char linha[256];
+        
+        if(fgets(linha, sizeof(linha), arquivo) == NULL) {
+            fprintf(stderr, "Error: Arquivo vazio ou falha ao ler o tempo total da simulacao.");
+            fclose(arquivo);
+            exit(1);
+
+        }
+
+        long tempoTotalSimulacao;
+        
+        if (!converter_long(linha, &tempoTotalSimulacao)) {
+            fprintf(stderr, "Error: Falha ao processar o tempo total de simulacao: %s\n", linha);
+            fclose(arquivo);
+            exit(1);
+        } else {
+            printf("Tempo total de simulacao: %ld\n", tempoTotalSimulacao);
+        }
+
+        while (fgets(linha, sizeof(linha), arquivo)) {
+            tarefa novaTarefa;
+
+            if (!parse_argumentos(linha, &novaTarefa)) {
+                fprintf(stderr, "Error: Falha ao processar a linha: %s\n", linha);
+                fclose(arquivo);
+                exit(1);
+            } else {
+                if (contadorTarefas >= 100) {
+                    fprintf(stderr, "Error: Lista de tarefas cheia.");
+                    exit(1);
+                }
+
+                strcpy(listaTarefas[contadorTarefas].nome, novaTarefa.nome);
+                listaTarefas[contadorTarefas].PERIODO = novaTarefa.PERIODO;
+                listaTarefas[contadorTarefas].BURST = novaTarefa.BURST;
+                listaTarefas[contadorTarefas].DEADLINE = novaTarefa.DEADLINE;
+                contadorTarefas++;
+
+                printf("Tarefa %d: %s, PERIODO: %ld, DEADLINE: %ld, BURST: %ld\n", contadorTarefas, novaTarefa.nome, novaTarefa.PERIODO, novaTarefa.DEADLINE, novaTarefa.BURST);
+            }
+            
+        }
+        
+        fclose(arquivo);
+    }
+
+
+    return 0;
+}
